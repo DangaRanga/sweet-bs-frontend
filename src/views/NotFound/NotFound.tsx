a1da34f30b4cf7cea5918c3f8db1b2c41130e2d6
DangaRanga <jasonblue1626@gmail.com>
2021-03-05 03:04:56 -0500

    started buttons and 404 page

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -3,14 +3,13 @@ import './NotFound.css';
 
 interface NotFoundProps {}
 
-interface NotFoundState {}
-
-export default class NotFound extends Component<NotFoundProps, NotFoundState> {
-    render() {
-        return (
-            <div>
-                <h1>404</h1>
-            </div>
-        );
-    }
+function NotFound() {
+    return (
+        <div id="not-found">
+            <h3 id="not-found-text"> There's nothing here</h3>
+            <img src="page_not_found.svg" id="not-found-img"></img>
+        </div>
+    );
 }
+
+export default NotFound;
