76810491f2567ec05e37e939088aeb82e625ffe1
DangaRanga <jasonblue1626@gmail.com>
2021-03-25 23:26:24 -0500

    More modifications to portal css

--- src/views/ManagementPortal/PortalSidebar/PortalSidebar.tsx
+++ src/views/ManagementPortal/PortalSidebar/PortalSidebar.tsx
@@ -11,9 +11,52 @@ function NavSidebar() {
     return (
         <aside id="nav-sidebar">
             <div id="sidebar-header">
-                <img src={LongLogo} id="sweetbs-logo" />
+                <img src={LongLogo} id="sweetbs-logo" alt="logo" />
             </div>
             <hr className="portal-divider" />
+            <ul id="portal-nav-items">
+                <li>
+                    <img
+                        src=""
+                        className="portal-nav-icon"
+                        onClick={() => {}}
+                    />
+                    <p>Dashboard</p>
+                </li>
+                <li>
+                    <img
+                        src=""
+                        className="portal-nav-icon"
+                        onClick={() => {}}
+                    />
+                    <p>Customer Analytics</p>
+                </li>
+                <li>
+                    <img
+                        src=""
+                        className="portal-nav-icon"
+                        onClick={() => {}}
+                    />
+                    <p>Order Analytics</p>
+                </li>
+                <li>
+                    <img
+                        src=""
+                        className="portal-nav-icon"
+                        onClick={() => {}}
+                    />
+                    <p>Manage Menu</p>
+                </li>
+                <li>
+                    <img
+                        src=""
+                        className="portal-nav-icon"
+                        onClick={() => {}}
+                    />
+                    <p>Shopping List</p>
+                </li>
+            </ul>
+            <div className="portal-divider-word">Controls</div>
         </aside>
     );
 }
